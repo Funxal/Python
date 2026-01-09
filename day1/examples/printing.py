1ae608a8e798eabcaa77f5c63134acb36fac4df7
Habibah omolara <habibah 049@gmail.com>
2026-01-09 11:06:06 +0100

    Initial commit

---
+++ day1/examples/printing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+name = "Habibah"
+age = 23
+height = 5.4
+is_student = True
+
+print(f'My name is {name}.\n'
+      f'I am {age} years old.\n'
+      f'I am {height}cm tall.\n'
+      f'It is {is_student} that I am a student.\n')
